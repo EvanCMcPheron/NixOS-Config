9d834011f68300945eacb48846a3b629e2159305
Evan McPheron <evanmcpheron@gmail.com>
2026-07-01 08:44:52 -0500

    Some Reorg

--- flake.nix
+++ flake.nix
@@ -2,14 +2,35 @@
   description = "Main Configuration Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.06";
-  };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-      ];
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs"; # locks inputs.nixpkgs to our nixpkgs url to ensure versioning consistancy
     };
   };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/nixos/
+
+          home-manager.nixosModules.home-manager 
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.etanheinmik = import ./home.nix;
+          }
+        ];
+
+      }
+    };
+
+  };
+
 }
