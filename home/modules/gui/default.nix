45fcf25348c4b6e6e433a7f5c234314544fe17bd
Evan McPheron <evancmcpheron@gmail.com>
2026-07-06 18:40:59 -0500

    Spread files out

---
+++ home/modules/gui/default.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+{
+	xdg.configFile."hypr".source = ./hypr;
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+    kdePackages.kate
+    google-chrome
+    ghostty
+    kitty
+    lazygit
+    fish
+    waybar
+    wofi
+    hypridle
+    hyprlock
+    hyprpaper
+    hyprshot
+    firefox
+  ];
+}
--- home/modules/terminal/default.nix
+++ home/modules/terminal/default.nix
@@ -1,8 +1,74 @@
 { config, pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    nnn # terminal file manager
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+  ];
+
   programs.git = {
     enable = true;
     userName = "Evan McPheron";
     userEmail = "evancmcpheron@gmail.com";
+  };
+
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+
+    bashrcExtra = ''
+      export QT_QPA_PLATFORMTHEME=gtk2
+
+      export PATH=/home/etanheinmik/repos/swww/target/release/:$PATH
+      export PATH=/home/etanheinmik/repos/swww/target/release/swww:$PATH
+      export PATH=/home/etanheinmik/repos/woomer/target/release/woomer:$PATH
+      export PATH=/home/etanheinmik/.cargo/bin/:$PATH
+      export PATH=/home/etanheinmik/classes/ece391/riscv/bin:$PATH
+
+      set -o vi
+    '';
+
+    # set some aliases, feel free to add more or remove some
+    shellAliases = {
+      ls="eza";
+      ll="eza -l";
+      python-venv="source ~/.pyvenv/bin/activate";
+      fzf="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'";
+      nmls="nmcli device wifi list";
+      nmcn="nmcli device wifi connect --ask";
+    };
   };
 }
